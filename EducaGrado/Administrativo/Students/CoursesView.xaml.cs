1b2dc5c026a721cb09855e0d819c742af989c4f1
projecte2021 <83172423+projecte2021@users.noreply.github.com>
2022-05-13 01:42:54 -0400

    Implement Interfaces

---
+++ EducaGrado/Administrativo/Students/CoursesView.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Implementation;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EducaGrado.Administrativo.Students
+{
+    /// <summary>
+    /// Lógica de interacción para CoursesView.xaml
+    /// </summary>
+    public partial class CoursesView : UserControl
+    {
+        Course course;
+        CourseImpl courseImpl;
+        public CoursesView()
+        {
+            InitializeComponent();
+        }
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            //LoadDataGrid();
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Ocultar();
+        }
+        public void LoadDataGrid()
+        {
+            //try
+            //{
+            //    courseImpl = new CourseImpl();
+            //    dgvDatos.ItemsSource = null;
+            //    dgvDatos.ItemsSource = courseImpl.Select().DefaultView;
+
+            //}
+            //catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        public void LoadDataGridlike(string like)
+        {
+            //try
+            //{
+            //    courseImpl = new CourseImpl();
+            //    dgvDatos.ItemsSource = null;
+            //    dgvDatos.ItemsSource = courseImpl.Selectlike(like).DefaultView;
+
+            //}
+            //catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        public void Ocultar()
+        {
+            //dgvDatos.Columns[0].Visibility = Visibility.Hidden;
+        }
+
+
+        private void DgvDatos_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            //if (dgvDatos.Items.Count > 0 && dgvDatos.SelectedItem != null)
+            //{
+            //    try
+            //    {
+
+            //        DataRowView dataRow = (DataRowView)dgvDatos.SelectedItem;
+            //        int id = int.Parse(dataRow.Row.ItemArray[0].ToString());
+
+            //        courseImpl = new CourseImpl();
+            //        course = courseImpl.Get(id);
+            //        lblcourse.Content = "" + course.Number;
+            //        lblletter.Content = course.Letter;
+            //        lblsection.Content = course.Section;
+            //        btnStu.IsEnabled = true;
+            //        btnSubs.IsEnabled = true;
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show(ex.Message);
+            //    }
+            //}
+        }
+
+        private void Txtsearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //try
+            //{
+            //    if (txtsearch.Text == "")
+            //    {
+            //        LoadDataGrid();
+            //        Ocultar();
+            //    }
+            //    else
+            //    {
+            //        LoadDataGridlike(txtsearch.Text);
+            //        Ocultar();
+            //    }
+            //}
+            //catch
+            //{
+
+            //}
+        }
+
+
+
+        private void BtnStu_Click(object sender, RoutedEventArgs e)
+        {
+            //Students s = new Students(course.Idcourse);
+            //s.Show();
+
+        }
+
+        private void BtnSubs_Click(object sender, RoutedEventArgs e)
+        {
+            //SubjectsList sl = new SubjectsList(course.Idcourse);
+            //sl.Show();
+        }
+    }
+}
