0ade28650800f3e5ecd3e73904441fa80494af9d
Aldo Lima <aldexkepex.al@gmail.com>
2022-05-18 00:44:51 -0400

    creacion de la ventana modificar colegio
    se creo la ventana para modificar colegio

---
+++ EducaGrado/Administrativo/School/ModifySchool.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EducaGrado.Administrativo.School
+{
+    /// <summary>
+    /// Lógica de interacción para ModifySchool.xaml
+    /// </summary>
+    public partial class ModifySchool : Window
+    {
+        public ModifySchool()
+        {
+            InitializeComponent();
+        }
+    }
+}
