0e26c3e163fb0e36480355326c71659af43f3e67
Aldo Lima <aldexkepex.al@gmail.com>
2022-05-26 22:23:39 -0400

    creacion de las views para profesor

---
+++ EducaGrado/Profesor/Home/TeacherHome.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EducaGrado.Profesor.Home
+{
+    /// <summary>
+    /// Lógica de interacción para TeacherHome.xaml
+    /// </summary>
+    public partial class TeacherHome : Window
+    {
+        public TeacherHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EducaGrado/Profesor/Subjects/StudenGrade.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EducaGrado.Profesor.Subjects
+{
+    /// <summary>
+    /// Lógica de interacción para StudenGrade.xaml
+    /// </summary>
+    public partial class StudenGrade : Window
+    {
+        public StudenGrade()
+        {
+            InitializeComponent();
+        }
+    }
+}
